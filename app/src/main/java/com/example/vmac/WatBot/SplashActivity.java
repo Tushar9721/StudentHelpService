9fded4716fdd930bcbbdcd911ea2aea2ef010467
Tushar Garg <tushargarg313@gmail.com>
2020-03-13 23:52:13 -0400

    Adding the view
    Adding the dialog view

--- app/src/main/java/com/example/vmac/WatBot/SplashActivity.java
+++ app/src/main/java/com/example/vmac/WatBot/SplashActivity.java
@@ -1,22 +1,35 @@
 package com.example.vmac.WatBot;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private ImageView floatingButton;
+    private Button walkSafe, mentalHealth, careerAdvisor, fitnessAdvisor, campusHelp, sexualHealth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        floatingButton = findViewById(R.id.floatingButton);
+        ImageView floatingButton = findViewById(R.id.floatingButton);
+        walkSafe = findViewById(R.id.btnWalkSafe);
+        mentalHealth = findViewById(R.id.btnMentalHealth);
+        careerAdvisor = findViewById(R.id.btnCareerAdvisor);
+        fitnessAdvisor = findViewById(R.id.btnFitnessAdvisor);
+        campusHelp = findViewById(R.id.btnCampusHelp);
+        sexualHealth = findViewById(R.id.btnSexualAssault);
+
+
+
 
         floatingButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,7 +40,80 @@ public class SplashActivity extends AppCompatActivity {
 
         });
 
+        walkSafe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                openDialog("https://uwsa.ca/uwsa-services/walksafe/");
+            }
+        });
+
+        mentalHealth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog("https://uwsa.ca/uwsa-services/walksafe/");
+
+
+            }
+        });
+
+
+        careerAdvisor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog("https://uwsa.ca/uwsa-services/walksafe/");
+
+
+            }
+        });
+
+
+
+        campusHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog("https://uwsa.ca/uwsa-services/walksafe/");
+
+
+            }
+        });
+
+
+
+        sexualHealth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog("https://uwsa.ca/uwsa-services/walksafe/");
+
+
+            }
+        });
+
+
+
+        fitnessAdvisor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openDialog("https://uwsa.ca/uwsa-services/walksafe/");
+
+            }
+        });
 
     }
+
+
+    private void openDialog(String url) {
+
+        Dialog dialog = new Dialog(this, android.R.style.Theme_Light);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_view);
+        dialog.setCancelable(true);
+        WebView browser = dialog.findViewById(R.id.webview);
+       browser.loadUrl(url);
+        dialog.show();
+
+    }
+
+
 }
